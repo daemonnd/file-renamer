5b9cfb9ad452a381c085f3b06dcb5cf02c73b0e2
daemonnd <daemonnd@protonmail.com>
2026-03-07 14:43:32 +0100

    enhance file renaming scripts by fixing small bugs and adding flags

--- file-renamer.sh
+++ file-renamer.sh
@@ -19,13 +19,42 @@ trap 'echo "Error on line $LINENO in file-renamer.sh: command \"$BASH_COMMAND\" 
 trap 'cleanup' INT TERM 
 
 function check_args {
-    echo "${1:?ERROR: A path to a list file containing dir paths have to be providen as first argument.}"
-    echo
+    : "${1:?ERROR: A path to a list file containing dir paths have to be providen as first argument.}"
+}
+function calc_output_mode {
+    # calculate the output mode with the -v and -s flags
+    output_mode=$(($output_mode+$1))
+}
+function parse_flags {
+    # parse the args
+    # s = silent (no output except errors) output_mode=-1
+    # default (no s nor v): only prints errors and the current dir processed in the given file output_mode=0
+    # v = all errors, all filenames from -> to changed output_mode=1
+    # vv = all errors, all filenames from --> to changed, also which filenames did not change output_mode=2
+    output_mode=0 
+    while getopts "sv" flag; do
+        case "${flag}" in
+            s) calc_output_mode -1;;
+            v) calc_output_mode 1;;
+        esac
+    done
+    shift "$((OPTIND-1))"
+    args="$@"
+}
+function log {
+    # logs the output depending on the output_mode
+    # args: 
+    # 1. loglevel (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+    # 2. logmessage
+    # 3. min. output_mode
+    if [[ "$3" -le "$output_mode" ]]; then
+        echo "${1}: $2"
+    fi
 }
 function file_exists {
     # check wether $1 exists or not, return 1 if not, return 0 if yes
     if [[ ! -e "$1" ]]; then 
-        echo "File ${1@Q} does not exist!"
+        log "ERROR" "File ${1@Q} does not exist!" -2
         return 1
     else
         return 0
@@ -34,40 +63,46 @@ function file_exists {
 function dir_exists {
     # check wether $1 exists or not, return 1 if not, return 0 if yes
     if [[ ! -d "$1" ]]; then
-        if [[ ! file_exists ]]; then
-            echo "ERROR: ${1@Q} does not exist!"
+        if ! file_exists "$1"; then
+            log "ERROR" "${1@Q} does not exist!" -2
         else
-            echo "ERROR: ${1@Q}1 is not a dir!"
+            log "{1@Q}1 is not a dir!"
         fi
         return 1
     else
+        if ! find "$1" -type d -empty | grep -q .; then
             return 0
+        else
+            echo "The dir $1 is empty!"
+            return 1
         fi
+    fi
     }
     function init {
         # initialize the script by checking if the dirs in the list file exist, and saving all the paths in an array
 
         # check if the list file exists
         if ! file_exists "$1"; then
-            echo "ERROR: The list file does not exist!"
+            log "ERROR" "The list file does not exist!" -2
             exit 1
         fi
 
         # save all the dir paths in an array
         mapfile -t dirlist < <(cat "$1")
 
-    for dir in "${dirlist[@]}"; do
-        dir_exists "$dir"
-    done
+        for dir in "${dirlist[@]}"; do
+            dir_exists "$dir"
+        done
 
 }
 
 function main {
-    check_args "$@"
-    init "$@"
+    parse_flags "$@"
+    check_args "$args"
+    init "$args"
     for dir in "${dirlist[@]}"; do
-        echo "current dir: ${dir@Q}"
-        find "${dir}" -mindepth 1 -depth -print0 | xargs -0 -L1 /usr/local/bin/rename_one_file.sh
+        log "INFO" "current dir: ${dir@Q}" 0
+        find "${dir}" -mindepth 1 -depth -print0 | xargs -0 -L1 /usr/local/bin/rename_one_file.sh "$output_mode"
     done
 }
 
--- rename_one_file.sh
+++ rename_one_file.sh
@@ -19,15 +19,38 @@ trap 'echo "Error on line $LINENO in rename_one_file.sh: command \"$BASH_COMMAND
 trap 'cleanup' INT TERM 
 
 function check_args {
-    echo "${1:?ERROR: An absolute filepath has to be gives as the first argument.}"
+    : "${1:?ERROR: The output mode has to be given as first argument (int from -1 to 2).}"
+    : "${2:?ERROR: An absolute filepath has to be gives as the first argument.}"  
+
+    # save the output mode
+    output_mode="$1"
+    args="$2"
 }
 function file_exists {
     # check wether $1 exists or not, return 1 if not, return 0 if yes
     if [[ ! -e "$1" ]]; then 
-        echo "File ${1@Q} does not exist!"
+        log "ERROR" "File ${1@Q} does not exist!" -2
         return 1
     else
         return 0
+    fi
+}
+function is_file {
+    # check wether the arg is a regular file or a dir
+    if [[ -f "$1" ]]; then 
+        return 0
+    else
+        return 1
+    fi
+}
+function log {
+    # logs the output depending on the output_mode
+    # args: 
+    # 1. loglevel (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+    # 2. logmessage
+    # 3. min. output_mode
+    if [[ "$3" -le "$output_mode" ]]; then
+        echo "${1}: $2"
     fi
 }
 function clean_name {
@@ -36,57 +59,65 @@ function clean_name {
     local improved_filename="$raw_filename"
 
     improved_filename="${improved_filename//[[:space:]]/_}"
-    blacklist_characters=(':' '´' '&' '#' '+' '\*' '~' '\(' '\)' '\[' '\]' '{' '}' ',' '\?')
+    blacklist_characters=(':' '´' '&' '#' '+' '\*' '~' '(' ')' '[' ']' '{' '}' ',' '\?' '@' ';' '=' '%') # * ( ) [ ] ? @
 
     # apply removing probelmatic characters
     for char in "${blacklist_characters[@]}"; do
         improved_filename="${improved_filename//$char}"
     done
 
-    # remove leading and trailing _ and -
-    improved_filename=$(echo "$improved_filename" | sed -E 's/^-+//')
-    improved_filename=$(echo "$improved_filename" | sed -E 's/^_+//')
-    improved_filename=$(echo "$improved_filename" | sed -E 's/_+$//')
-    improved_filename=$(echo "$improved_filename" | sed -E 's/-+$//')
+    # remove leading and trailing _ and - and trailing .
+    improved_filename=$(echo "$improved_filename" | sed -E 's/^-+//; s/^_+//; s/_+$//; s/-+$//; s/\.$//')
 
-    # add file creation date to the beginning if it does not exist
-    if [[ ! "$improved_filename" =~ '^[0-9]{4}-[0-9]{2}-[0-9]{2}'  ]]; then
-        local file_creation_date="$(stat -c '%w' "$1" | awk ' { print $1 } ' )"
-        # only add the creation date to the beginning of the file if it is not '-'
-        if [[ "$file_creation_date" != '-' ]]; then
-            improved_filename="${file_creation_date}_${improved_filename}"
+    # add file creation date to the beginning if it does not exist and it is a regular file
+    if is_file "$1"; then
+        if [[ ! "$improved_filename" =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}  ]]; then
+            local file_creation_date="$(stat -c '%w' "$1" | awk ' { print $1 } ' )"
+            # only add the creation date to the beginning of the file if it is not '-'
+            if [[ "$file_creation_date" != '-' ]]; then
+                improved_filename="${file_creation_date}_${improved_filename}"
+            fi
         fi
     fi
 
     # replace __ with _
     improved_filename="${improved_filename//__/_}"
 
+    # check wether $improved_filename is empty (only contains the date)
+    if [[ "$improved_filename" =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}_$ ]]; then
+        log "WARNING" "Counld not clean the name of ${raw_filename}, because it would be empty. It will stay as it is, so please rename it manually." 0
+        improved_filename="$raw_filename"
+    elif [[ -z "$improved_filename" ]]; then
+        log "WARNING" "Counld not clean the name of ${raw_filename}, because it would be empty. It will stay as it is, so please rename it manually." 0
+        improved_filename="$raw_filename"
+    fi
+
     # save destination path & actually rename the file
     local dest_path="${1%/*}/${improved_filename}"
-    echo "dest_path: $dest_path"
     if [[ "$1" == "$dest_path" ]]; then
-        echo "$1 and $dest_path are the same. $1 seems to have a non-problematic name already."
+        log "INFO" "$1 and $dest_path are the same. $1 seems to have a non-problematic name already." 2
     else
-        echo "Renaming $1 -> $dest_path"
+        log "INFO" "Renaming $1 -> $dest_path" 1
         if ! mv "$1" "$dest_path"; then
-            echo "ERROR while executing `mv $1 $dest_path`."
+            log "ERROR" "ERROR while executing: mv $1 $dest_path." -2
             false
         fi
     fi
 }
 function init {
-    # initialize the script by checking if the dirs in the list file exist, and saving all the paths in an array
-
+    # save the output mode
+    output_mode="$1"
+    args="$2"
     # check if the list file exists
-    if ! file_exists "$1"; then
-        echo "ERROR: The list file does not exist!"
+    if ! file_exists "$args"; then
+        log "ERROR" "The list file does not exist!" -2
         exit 1
     fi
 }
 
 function main {
-    init "$1"
-    clean_name "$1"
+    init "$@"
+    clean_name "$args"
 }
 
 # check if a filename has been gives as the first argument
